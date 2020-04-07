d702f746d11edab525e493c8d7bc4c3b15a6d19f
Dominic Modderman <djmodd@live.com>
2020-04-07 19:17:03 -0400

    RiggedALeg

---
+++ Assets/Scripts/PositionAtHalf.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PositionAtHalf : MonoBehaviour
+{
+
+    public Transform tOne;
+    public Transform tTwo;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (tOne != null && tTwo != null)
+        {
+            Vector3 tOneToTTwo =  tTwo.position - tOne.position;
+            Vector3 newPos = (Vector3.Normalize(tOneToTTwo) * (tOneToTTwo.magnitude/2)) + tOne.position;
+            transform.position = newPos;
+        }
+    }
+}
---
+++ Assets/Scripts/WalkCycleTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkCycleTest : MonoBehaviour
+{
+    public Transform Hips;
+    float LegLength = 1000;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = FindPointUnderHips();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Hips.position, Vector3.down, new Color(255, 0, 0,255), 100000f, false);
+    }
+
+
+    void CalcGate() { 
+    
+    
+    }
+
+    //TODO: make this relaitce to the body
+    Vector3 FindPointUnderHips() {
+        
+        RaycastHit hit = RaycastFromHips();
+        if (hit.distance > LegLength)
+        {
+            return transform.position;
+        }
+        else {
+            return hit.point;
+        }
+
+    }
+
+    RaycastHit RaycastFromHips(Vector3 direction) {
+        if (Hips == null) {
+            print("ERROR: hips not set");
+            return new RaycastHit();
+        }
+        Ray FromHips = new Ray(Hips.position, direction);
+        Debug.DrawRay(Hips.position, direction, new Color(255,0,0), 100000f,false);
+        Physics.Raycast(FromHips, out RaycastHit hit);
+        print(hit);
+        return hit;
+    }
+
+    RaycastHit RaycastFromHips()
+    {
+        return RaycastFromHips(Vector3.down);
+    }
+
+
+
+    public float WalkCurve(float x)
+    {
+        float y = Mathf.Sin(x * Mathf.PI);
+
+        return WalkCurve(x, y);
+    }
+    public float WalkCurve(float x,float y) {
+
+
+        return Mathf.Sin(x * Mathf.PI - y);
+    }
+
+   
+}
